7aa7d0993b04d6759fa1a1953ec71efc4ef5c415
Rumi Cain <rumicain@MacBook-Air-de-Rumi.local>
2026-04-07 02:01:04 +0200

    fix base path + logo import from src

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/javierbosco/',     // ← Este es el correcto para tu repo
+  base: '/javierbosco/',     // 
 })
